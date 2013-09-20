216c48622fac1cc2fa1771f77240fa1ea992d8e5
Zul <Zulus@Zuls-Mac.local>
2013-09-20 03:54:37 -0700

    f1123456783

--- Mosq.m
+++ Mosq.m
@@ -10,4 +10,15 @@
 
 @implementation Mosq
 
+-(id) init {
+    
+	if( (self=[super init]) ) {
+		
+        me = [CCSprite spriteWithFile:@"mos_fly_ w (1).png"];
+        me.position = ccp(0,0);
+        [self addChild:me];
+        
+	}
+	return self;
+}
 @end
